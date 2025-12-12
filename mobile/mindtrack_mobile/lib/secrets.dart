ed8cff99eb13770f0813be33312366f6557df68e
Saiganeshk15 <saiganeshk15@gmail.com>
2025-12-12 18:25:56 +0530

    Added dependencies

---
+++ mobile/mindtrack_mobile/lib/secrets.dart
@@ -0,0 +1,4 @@
+class AppSecrets {
+  static const supabaseUrl = "https://yavcbnxhfrwzfgxbpxyg.supabase.co";
+  static const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlhdmNibnhoZnJ3emZneGJweHlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU1MjE4NjMsImV4cCI6MjA4MTA5Nzg2M30.Du44FaUXP793aTNKA34Y7hGTIn7_ZObfILKjPrUYWNk";
+}
